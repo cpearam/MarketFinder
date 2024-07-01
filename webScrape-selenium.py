aeb98c54d06e13279b305ea864b97d54b98bd000
cpearam <rambertapolto@gmail.com>
2024-07-02 01:27:12 +0800

    import libraries needed and small testing

--- webScrape-selenium.py
+++ webScrape-selenium.py
@@ -1,3 +1,20 @@
-print("hello world")
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
-print("lets gooooo!")
+service = Service(executable_path="chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+
+driver.get("https:/lazada.com.ph")
+
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.ID, "q"))
+)
+
+search_bar = driver.find_element(By.ID, "q")
+search_bar.send_keys("yoyo" + Keys.ENTER)
+time.sleep(10)
